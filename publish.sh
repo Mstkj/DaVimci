f2377f3432612a6a01a843caa53b22b09d648390
Mstkj <jorinryms@gmail.com>
2021-02-18 19:40:18 -0500

    squashed bugs

--- publish.sh
+++ publish.sh
@@ -8,19 +8,19 @@ select menu in "${options[@]}"
 do
 	if [[ "$REPLY" = "1" ]]; then # Docx
 		echo -e "$REPLY is ${options[0]}.\n"
-		inp="${options[0]}" && REPLY="Docx"; SearchInput
+		in="${options[0]}" && REPLY="Docx"; SearchInput
 	elif [[ "$REPLY" = "2" ]]; then # PDF
 		echo -e "$REPLY is ${options[1]}.\n"
-		inp="${options[1]}" && REPLY="PDF"; SearchInput
+		in="${options[1]}" && REPLY="PDF"; SearchInput
 	elif [[ "$REPLY" = "3" ]]; then # Markdown
-		printf "\n%s${options[2]}.\n" && FORM_IN="${options[2]}" # DEBUG
-		inp="${options[2]}" && REPLY="Markdown"; SearchInput
+		printf "\n%s${options[2]}.\n"
+		in="${options[2]}" && REPLY="Markdown"; SearchInput
 	elif [[ "$REPLY" = "4" ]]; then # LaTeX
 		echo -e "$REPLY is ${options[3]}.\n"
-		inp="${options[3]}" && REPLY="LaTeX"; SearchInput
+		in="${options[3]}" && REPLY="LaTeX"; SearchInput
 	elif [[ "$REPLY" = "5" ]]; then # HTML5
 		echo -e "$REPLY is ${options[4]}.\n"
-		inp="${options[4]}" && REPLY="HTML5"; SearchInput
+		in="${options[4]}" && REPLY="HTML5"; SearchInput
 	else
 		clear ; echo -e "invalid option.\n"; Main
 	fi
@@ -68,8 +68,11 @@ do
 		echo -e "Option $REPLY is ${options[1]}.\n"
 		out="${options[1]}"; PdfEngine
 	elif [[ "$REPLY" = "3" ]]; then # Markdown
-		FORM_OUT="${options[2]}" && printf "\nOutput set to %s${options[2]}.\n\n" # DEBUG
+
+		#FORM_OUT="${options[2]}"
+		printf "\nOutput set to %s${options[2]}.\n\n" # DEBUG
 		out="${options[2]}"; PdfEngine
+
 	elif [[ "$REPLY" = "4" ]]; then # LaTeX
 		# TODO: we're assuming that  LaTeX is an actual .tex LaTeX document and not a PDF. <18-02-21, melthsked> #
 		echo -e "Option $REPLY is ${options[3]}.\n"
@@ -86,7 +89,7 @@ do
 done
 
 printf "Name your document:~$ "; read -rp "" name
-OUTPUT="$name.$out"
+OUTPUT="$name."
 printf "%s$OUTPUT.\n"
 # TODO: If output LaTeX, publish to Tex or PDF? <16-02-21, melthsked> #
 }
@@ -189,7 +192,7 @@ function ArticleClass() {
 function PandocOutputCommand() {
 # TODO: First, verify which variables are being used. <16-02-21, melthsked> #
 # TODO: Final pandoc command function goes here; it should be a giant if statement. <16-02-21, melthsked> #
-pandoc "$defaults" -f "$FORM_IN" -t "$FORM_OUT" "$INPUT" "$engine" "$template" "$css" "$metadata" --highlight-style=monochrome -V "$class" -V papersize=A4 --indented-code-classes=javascript --verbose --strip-comments --standalone --log=debug.log --data-dir=./ -o "${OUTPUT}"
+pandoc "$defaults" -f "$in" -t "$out" "$INPUT" "$engine" "$template" "$css" "$metadata" --highlight-style=monochrome -V "$class" -V papersize=A4 --indented-code-classes=javascript --verbose --strip-comments --standalone --log=debug.log --data-dir=./ -o "${OUTPUT}"
 }
 
 Main "$@" || [[ -z "${!$?}" ]] && print Failed ; exit 1
